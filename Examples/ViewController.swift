a80af258a2a996ffeaff6cc70053cdd3679327a7
Vitalii Havryliuk <v.havryliuk@noisyminer.com>
2019-09-25 08:49:12 +0300

    Initial project setup

---
+++ Examples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Examples
+//
+//  Created by Vitalii Havryliuk on 25.09.2019.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
