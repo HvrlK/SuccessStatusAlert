10f6f792be7915165332a34c7d5569887d3e40fc
Vitalii Havryliuk <v.havryliuk@noisyminer.com>
2019-09-25 08:39:16 +0300

    Initial Commit

---
+++ SuccessStatusAlert/SuccessStatusAlert.h
@@ -0,0 +1,18 @@
+//
+//  SuccessStatusAlert.h
+//  SuccessStatusAlert
+//
+//  Created by Vitalii Havryliuk on 25.09.2019.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SuccessStatusAlert.
+FOUNDATION_EXPORT double SuccessStatusAlertVersionNumber;
+
+//! Project version string for SuccessStatusAlert.
+FOUNDATION_EXPORT const unsigned char SuccessStatusAlertVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SuccessStatusAlert/PublicHeader.h>
+
+
